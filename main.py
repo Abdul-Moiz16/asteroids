af70bda689f81f205f9374c23ba4171f979c8536
Abdul-Moiz16 <imoizsiddiqui@gmail.com>
2026-07-14 16:27:20 +0200

    groups refactor

--- main.py
+++ main.py
@@ -5,27 +5,31 @@ from constants import SCREEN_WIDTH
 from logger import log_state
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0.0
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        screen.fill("black")
+
         for event in pygame.event.get():
             log_state()
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        for update in updatable:
+            update.update(dt)
+        
+        for draw in drawable:
+            draw.draw(screen)
         dt = clock.tick(60) / 1000.0
-        player.draw(screen)   
-        player.update(dt)
         pygame.display.flip()
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
